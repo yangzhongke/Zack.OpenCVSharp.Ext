375d91ab50b292721b81d57d88d82eb3e38a4450
yzk <yzk365@qq.com>
2020-12-15 05:38:28 +0800

    init

---
+++ np.cs
@@ -0,0 +1,35 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zack.OpenCVSharp.Ext
+{
+    public static class np
+    {
+        //https://stackoverflow.com/questions/52016253/python-np-array-equivilent-in-opencv-opencvsharp
+        public static Mat zeros_like(Mat a)
+        {
+            return new Mat(a.Size(), a.Type(),new Scalar(0));
+        }
+        public static Mat array(byte[] array_like)
+        {
+            return Mat.FromArray(array_like);
+        }
+
+        public static Mat array(Vec3b[] array_like)
+        {
+            return Mat.FromArray(array_like);
+        }
+
+        public static Mat array(Vec2b[] array_like)
+        {
+            return Mat.FromArray(array_like);
+        }
+
+        public static Mat array(Vec4b[] array_like)
+        {
+            return Mat.FromArray(array_like);
+        }
+    }
+}
